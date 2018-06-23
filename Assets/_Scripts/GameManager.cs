4b7b26e78ce8bd9363073122c21777797e179762
viktorianoo <40516900+viktorianoo@users.noreply.github.com>
2018-06-23 13:42:29 +0200

    v1Prototypo

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,23 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-	public static myGrid grid;
+    public static myGrid grid;
+    private float count;
+    public static int score;
+    public Text scoreText;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        count = 0;
+    }
 
-	public static void SetGrid(myGrid _grid)
-	{
-		grid = _grid;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        count = count + Time.deltaTime;
+        score = (int)count;
+        SetScoreText();
+    }
+
+    public static void SetGrid(myGrid _grid)
+    {
+        grid = _grid;
+    }
+    void SetScoreText()
+    {
+        scoreText.text = score.ToString();
+    }
 }
